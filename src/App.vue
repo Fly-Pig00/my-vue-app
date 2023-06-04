1dfa1cf53af6531e77014a33f5be0fd0a9f75f5c
fly-pig00 <layton.hart.cool@gmail.com>
2023-06-04 11:36:52 +0200

    first page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
 import Sidebar from './components/Sidebar.vue';
+import MainPanel from './components/MainPanel.vue';
 import './assets/scss/style.scss';
-
 </script>
 
 <template>
-  
-<Sidebar></Sidebar>
+  <div class="app">
+    <div class="container">
+      <img src="logo.svg" alt="logo">
+      <div class="title">
+        <b>Application</b> Information
+      </div>
+      <div class="sub-container">
+        <Sidebar></Sidebar>
+        <MainPanel></MainPanel>
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,9 +2,28 @@
 </script>
 
 <template>
-<div class="sidebar">
-  <div class="sidebar_numbers">
-    <div class="number"></div>
+  <div class="sidebar">
+    <div class="numbers">
+      <div class="number active">1</div>
+      <div class="dot-blue"></div>
+      <div class="dot-gray"></div>
+      <div class="dot-white"></div>
+      <div class="number">2</div>
+      <div class="number">3</div>
+      <div class="number">4</div>
+      <div class="number">5</div>
+      <div class="number">6</div>
+    </div>
+    <div class="content">
+      <div class="first">Personal information</div>
+      <div class="text-blue">Basic Information</div>
+      <div class="text-gray">European physical presence</div>
+      <div class="text-gray submit">Submit Documents</div>
+      <div class="num-content product-type">Product type</div>
+      <div class="num-content">Delivery time</div>
+      <div class="num-content">Customer Service</div>
+      <div class="num-content">Boutique information</div>
+      <div class="num-content">Payout</div>
+    </div>
   </div>
-</div>
 </template>
